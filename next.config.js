ec0b2ca6ebaa167da70e987679fb9d57e7d63469
JaeHoon-Shin <sinhoon0805@naver.com>
2023-03-06 13:41:36 +0900

    first

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 const path = require('path'); // 1. path 선언
 
 const nextConfig = {
+  basePath: "/https://github.com/moitnyang/moitnyang.git}",
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
