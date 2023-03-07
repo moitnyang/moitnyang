c98cd2fe669ee78b953966a2172066ef54a8d082
JaeHoon-Shin <sinhoon0805@naver.com>
2023-03-07 14:36:22 +0900

    test

--- pages/src/Login.js
+++ pages/src/Login.js
@@ -42,12 +42,12 @@ export default function Login() {
 
   //Google Handler function
   async function handleGoogleSignin(){
-    signIn("google",{callbackUrl:"http://localhost:3000"}) /* 메인페이지 주소로 나중에 바꾸기 */
+    /*signIn("google",{callbackUrl:"http://localhost:3000"})  메인페이지 주소로 나중에 바꾸기 */
   }
 
   //Github Login
   async function handleGithubSignin(){
-    signIn("github", {callbackUrl:"http://localhost:3000"})
+   /*  signIn("github", {callbackUrl:"http://localhost:3000"}) */
   }
 
   return (
