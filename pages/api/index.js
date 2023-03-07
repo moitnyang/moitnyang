0a915725a60d320c22054c90951bbabebbc15c51
JaeHoon-Shin <sinhoon0805@naver.com>
2023-03-07 16:39:57 +0900

    test

--- pages/api/index.js
+++ pages/api/index.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
   const {body,method} =req;
   
   
-  res.status(200).json(process.env.GITHUB_ID)
+  res.status(200).json(process.env.NEXT_PUBLIC_USER)
 
  
 
