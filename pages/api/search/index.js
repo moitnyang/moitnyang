26aaee88cd5d15ff3a31320ace34645d6e020edd
JaeHoon-Shin <sinhoon0805@naver.com>
2023-03-17 17:50:25 +0900

    검색데이터순위변경

--- pages/api/search/index.js
+++ pages/api/search/index.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     const { method, query } = req;
     const search = query.search;
     if (method === "GET") {
-        const searchData = await executeQuery(`select * from product where product_title like '%${search}%' or product_content like '%${search}%' or member_id like '%${search}%' or product_dong like '%${search}%' `, [])
+        const searchData = await executeQuery(`select * from product where product_title like '%${search}%' or product_content like '%${search}%' or member_id like '%${search}%' or product_dong like '%${search}%' ORDER BY product_no DESC `, [])
         res.status(200).json(searchData)
     }
 }
